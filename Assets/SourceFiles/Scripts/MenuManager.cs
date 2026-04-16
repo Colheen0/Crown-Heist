75645c55dde73d1a6d33f67d8669194d87a0e04f
Colheen0 <colheenn@gmail.com>
2026-04-16 10:28:47 +0200

    commenting fr finis

--- Assets/SourceFiles/Scripts/MenuManager.cs
+++ Assets/SourceFiles/Scripts/MenuManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//ici on gère les fonctions du menu principal (lancer le jeu et quitter le jeu)
 public class MainMenuManager : MonoBehaviour
 {
     public void LancerLeJeu()
